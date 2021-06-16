79037112fb916d75281ad5c2c510c1720c1eb13f
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-16 15:13:29 +0700

    Update private.py

--- MusicMan/modules/private.py
+++ MusicMan/modules/private.py
@@ -55,7 +55,7 @@ Gunakan Perintah » /help « Untuk Mengetahui Fitur Lengkap Saya
                         "ᴄʜᴀɴɴᴇʟ 📢", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "⚔️ ʙᴀɴᴛᴜᴀɴ", callback_data = f"help+{pos-1}"),
+                        "⚔️ ʙᴀɴᴛᴜᴀɴ", callback_data = "help+"),
                     InlineKeyboardButton(
                         "ᴅᴏɴᴀsɪ 🎁", url=f"https://saweria.co/DonasiUntukAdmin")
                 ],[
