7423cc212d915780b90b9aff39e866896c829b7c
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-28 08:26:23 +0700

    Update msg.py

--- MusicMan/modules/msg.py
+++ MusicMan/modules/msg.py
@@ -15,24 +15,52 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
-from MusicMan.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,DURATION_LIMIT, OWNER
+from MusicMan.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL, OWNER
 class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hai Selamat datang kembali di {PROJECT_NAME}
+**Hey 👋 Selamat datang kembali di {PROJECT_NAME}
+
 ✣️ {PROJECT_NAME} dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah.
+
 ✣️ Assistant Music » @{ASSISTANT_NAME}\n\nKlik Next untuk instruksi**
 
 """,
 
 f"""
-<b>👋🏻 Haii {message.from_user.mention} Nama Saya Adalah {PROJECT_NAME}
+**Pengaturan**
 
-Saya adalah bot canggih yang dibuat untuk memutar musik di obrolan suara Grup Telegram.
+1. Jadikan bot sebagai admin
+2. Mulai obrolan suara / VCG
+3. Ketik `/ubotjoin` Di Grup
+× Jika Assistant Bot bergabung selamat menikmati musik, 
+× Jika Assistant Bot tidak bergabung Silahkan Tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
 
-Dikelolah Oleh: {OWNER}
 
-Tekan Tombol Bantuan Untuk Mengetahui Fitur Lengkap Saya</b>
+**» Perintah Untuk dalam grup Member Juga Bisa :**
+
+ × /playlist : Untuk Menampilkan daftar putar Lagu sekarang
+ × /current : Untuk Menunjukkan  Lagu sekarang yang sedang diputar
+ × /song <judul lagu> : Untuk Mendownload lagu di YouTube 
+ × /video <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
+ × /vsong <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
+ × /deezer <judul lagu> : Untuk Mendownload lagu dari deezer 
+ × /saavn <judul lagu> : Untuk Mendownload lagu dari website saavn
+ × /search <judul lagu> : Untuk Mencari Video di YouTube dengan detail
+
+**» Perintah Hanya Untuk Admin :**
+
+× /play <judul lagu> : Untuk Memutar lagu yang Anda minta melalui youtube
+× /play <link yt> : Untuk Memutar lagu yang Anda minta melalui link youtube
+× /play <reply ke audio> : Untuk Memutar lagu yang Anda minta melalui file audio
+× /dplay : Untuk Memutar lagu yang Anda minta melalui deezer
+× /splay : Untuk Memutar lagu yang Anda minta melalui jio saavn
+× /skip : Untuk Menskip pemutaran lagu ke Lagu berikutnya
+× /pause : Untuk Menjeda pemutaran Lagu
+× /resume : Untuk Melanjutkan pemutaran Lagu yang di pause
+× /end : Untuk Memberhentikan pemutaran Lagu
+× /userbotjoin - Untuk Mengundang asisten ke obrolan Anda
+× /admincache - Untuk MemRefresh admin list
 """
       ]
