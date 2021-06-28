18f2464eaaa28cf60244bbbdb433a430056c0bb7
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-28 10:37:38 +0700

    Update private.py

--- MusicMan/modules/private.py
+++ MusicMan/modules/private.py
@@ -32,8 +32,6 @@ async def start_(client: Client, message: Message):
 Saya adalah bot canggih yang dibuat untuk memutar musik di obrolan suara Grup Telegram.
 
 Dikelolah Oleh: {OWNER}
-
-Tekan Tombol Bantuan Untuk Mengetahui Fitur Lengkap Saya
 </b>""",
 
         reply_markup=InlineKeyboardMarkup(
