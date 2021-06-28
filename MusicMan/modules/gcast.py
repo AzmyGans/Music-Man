5e99185f1a66b4f0c106671608e01693ef8cdf0c
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-28 12:35:02 +0700

    Update gcast.py

--- MusicMan/modules/gcast.py
+++ MusicMan/modules/gcast.py
@@ -25,18 +25,20 @@ async def bye(client, message):
     sent=0
     failed=0
     if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("`Globally Broadcasting Msg...`")
+        lol = await message.reply("Starting Gcast")
         if not message.reply_to_message:
-            await lol.edit("**Balas pesan teks apa pun untuk gcast**")
+            await lol.edit("Reply to any text message to gcast sir")
             return
         msg = message.reply_to_message.text
-        for dialog in client.iter_dialogs():
+        async for dialog in client.iter_dialogs():
             try:
                 await client.send_message(dialog.chat.id, msg)
                 sent = sent+1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
-            await asyncio.sleep(0.7)
-        await message.reply_text(f"**Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await asyncio.sleep(0.7)
+                
+        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
