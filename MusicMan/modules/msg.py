c043d73e6164dd719fb26fc3b12ebfd0d404ed6b
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-28 10:41:11 +0700

    Update msg.py

--- MusicMan/modules/msg.py
+++ MusicMan/modules/msg.py
@@ -20,47 +20,42 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Selamat datang kembali di {PROJECT_NAME}
+**👋🏻 Haii {message.from_user.mention} Nama Saya Adalah {PROJECT_NAME}\n
+Saya adalah bot canggih yang dibuat untuk memutar musik di obrolan suara Grup Telegram.
 
-✣️ {PROJECT_NAME} dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah.
-
-✣️ Assistant Music » @{ASSISTANT_NAME}\n\nKlik Next untuk instruksi**
+Dikelolah Oleh: {OWNER}**
 
 """,
 
 f"""
-**Pengaturan**
+**CARA MENGGUNAKAN BOT
+1. Jadikan Bot Sebagai Admin
+2. Mulai Obrolan Suara / VCG
+3. Ketik Perintah /ubotjoin
+• Jika Assistant Bot Bergabung Selamat Menikmati Musik, 
+• Jika Assistant Bot Tidak Bergabung Silahkan Tambahkan @{ASSISTANT_NAME} Ke Grup Anda Dan Coba Lagi
 
-1. Jadikan bot sebagai admin
-2. Mulai obrolan suara / VCG
-3. Ketik `/ubotjoin` Di Grup
-× Jika Assistant Bot bergabung selamat menikmati musik, 
-× Jika Assistant Bot tidak bergabung Silahkan Tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
+PERINTAH UNTUK SEMUA MEMBER GROUP
+• /play (judul lagu) : Untuk Memutar lagu yang Anda minta melalui youtube
+• /dplay (judul lagu) : Untuk Memutar lagu yang Anda minta melalui deezer
+• /splay (judul lagu) : Untuk Memutar lagu yang Anda minta melalui jio saavn
+• /ytplay (judul lagu) : Untuk Memutar lagu yang Anda minta melalui youtube dengan lebih detail
+• /search (judul lagu) : cari url youtube dengan detail
+• /playlist : Tampilkan sekarang daftar putar
+• /current : Tampilkan sekarang bermain
 
+PERINTAH UNTUK SEMUA ADMIN GROUP
+• /player : menampilkan panel pemutaran
+• /skip : Untuk Menskip pemutaran lagu ke Lagu berikutnya
+• /pause : Untuk Menjeda pemutaran Lagu
+• /resume : Untuk Melanjutkan pemutaran Lagu yang di pause
+• /end : Untuk Memberhentikan pemutaran Lagu
+• /ubotjoin : Untuk Mengundang asisten ke obrolan Anda
 
-**» Perintah Untuk dalam grup Member Juga Bisa :**
-
- × /playlist : Untuk Menampilkan daftar putar Lagu sekarang
- × /current : Untuk Menunjukkan  Lagu sekarang yang sedang diputar
- × /song <judul lagu> : Untuk Mendownload lagu di YouTube 
- × /video <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /vsong <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /deezer <judul lagu> : Untuk Mendownload lagu dari deezer 
- × /saavn <judul lagu> : Untuk Mendownload lagu dari website saavn
- × /search <judul lagu> : Untuk Mencari Video di YouTube dengan detail
-
-**» Perintah Hanya Untuk Admin :**
-
-× /play <judul lagu> : Untuk Memutar lagu yang Anda minta melalui youtube
-× /play <link yt> : Untuk Memutar lagu yang Anda minta melalui link youtube
-× /play <reply ke audio> : Untuk Memutar lagu yang Anda minta melalui file audio
-× /dplay : Untuk Memutar lagu yang Anda minta melalui deezer
-× /splay : Untuk Memutar lagu yang Anda minta melalui jio saavn
-× /skip : Untuk Menskip pemutaran lagu ke Lagu berikutnya
-× /pause : Untuk Menjeda pemutaran Lagu
-× /resume : Untuk Melanjutkan pemutaran Lagu yang di pause
-× /end : Untuk Memberhentikan pemutaran Lagu
-× /userbotjoin - Untuk Mengundang asisten ke obrolan Anda
-× /admincache - Untuk MemRefresh admin list
+CATATAN AGAR TIDAK TERJADI KENDALA
+• Untuk Menghindari Bot Error Jangan Melakukan Spam Musik Ke Dalam Antrian Sekaligus
+• Lagu Yang Melebihi Waktu 120 Menit Tidak Dapat Diputar
+• Jika Assistant Tidak Mau Naik Ke Obrolan Suara, Matiin Obrolan Suara Dan Mulai Lagi 
+• Itu Saja Pesan Dari Saya Terimakasih, Selamat Bermusik**
 """
       ]
