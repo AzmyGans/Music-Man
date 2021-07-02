5b11b3f103b4c91edb895cda8be2e806c51dd590
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-02 12:52:25 +0700

    Update __init__.py

--- MusicMan/services/callsmusic/__init__.py
+++ MusicMan/services/callsmusic/__init__.py
@@ -1,2 +1,6 @@
-from MusicMan.services.callsmusic import queues
-from MusicMan.services.callsmusic.callsmusic import pytgcalls, run
+from pyrogram import Client
+
+from MusicMan import config
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
