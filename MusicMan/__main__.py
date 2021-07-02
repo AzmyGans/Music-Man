ed1322679bdf48b1aa8af097faec924345eece75
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-02 12:35:43 +0700

    Update __main__.py

--- MusicMan/__main__.py
+++ MusicMan/__main__.py
@@ -18,14 +18,15 @@
 import requests
 from pyrogram import Client as Bot
 
-from MusicMan.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from MusicMan.config import API_HASH
+from MusicMan.config import API_ID
+from MusicMan.config import BG_IMAGE
+from MusicMan.config import BOT_TOKEN
 from MusicMan.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
-file = open("./etc/foreground.png", "wb")
-file.write(response.content)
-file.close()
-
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
 bot = Bot(
     ":memory:",
     API_ID,
