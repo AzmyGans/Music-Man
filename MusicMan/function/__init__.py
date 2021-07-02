6d1bf79aa55156151084950f795ef64971af8602
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-02 12:59:45 +0700

    Update __init__.py

--- MusicMan/function/__init__.py
+++ MusicMan/function/__init__.py
@@ -1,3 +1,5 @@
-from MusicMan.function.admins import admins, get, set
+from MusicMan.function.admins import admins
+from MusicMan.function.admins import get
+from MusicMan.function.admins import set
 
 __all__ = ["set", "get", "admins"]
