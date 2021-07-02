dd2a6cefb2c9652a74bc779403026208f4476c53
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-02 12:34:59 +0700

    Update channeladmin.py

--- MusicMan/modules/channeladmin.py
+++ MusicMan/modules/channeladmin.py
@@ -15,17 +15,20 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from asyncio.queues import QueueEmpty
-from MusicMan.config import que
-from pyrogram import Client, filters
+from asyncio import QueueEmpty
+from pyrogram import Client
+from pyrogram import filters
 from pyrogram.types import Message
 
+from MusicMan.config import que
 from MusicMan.function.admins import set
 from MusicMan.helpers.channelmusic import get_chat_id
-from MusicMan.helpers.decorators import authorized_users_only, errors
-from MusicMan.helpers.filters import command, other_filters
+from MusicMan.helpers.decorators import authorized_users_only
+from MusicMan.helpers.decorators import errors
+from MusicMan.helpers.filters import command 
+from MusicMan.helpers.filters import other_filters
 from MusicMan.services.callsmusic import callsmusic
-
+from MusicMan.services.queues import queues
 
 
 @Client.on_message(filters.command(["channelpause","cpause"]) & filters.group & ~filters.edited)
@@ -37,16 +40,16 @@ async def pause(_, message: Message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply("**Apakah obrolan terhubung?**")
+      await message.reply("Is chat even linked")
       return    
     chat_id = chid
-    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
-        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    if (chat_id not in callsmusic.active_chats) or (
+        callsmusic.active_chats[chat_id] == "paused"
     ):
-        await message.reply_text("❗ **Nothing is playing!**")
+        await message.reply_text("❗ Nothing is playing!")
     else:
-        callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("▶️ **Paused!**")
+        callsmusic.pause(chat_id)
+        await message.reply_text("▶️ Paused!")
 
 
 @Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
@@ -58,16 +61,16 @@ async def resume(_, message: Message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply("**Apakah obrolan terhubung?**")
+      await message.reply("Is chat even linked")
       return    
     chat_id = chid
-    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
-        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    if (chat_id not in callsmusic.active_chats) or (
+        callsmusic.active_chats[chat_id] == "playing"
     ):
-        await message.reply_text("❗ **Tidak ada Lagu yang sedang diputar!**")
+        await message.reply_text("❗ Nothing is paused!")
     else:
-        callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("⏸ **Resumed!**")
+        callsmusic.resume(chat_id)
+        await message.reply_text("⏸ Resumed!")
 
 
 @Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
@@ -79,19 +82,19 @@ async def stop(_, message: Message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply("**Apakah obrolan terhubung?**")
+      await message.reply("Is chat even linked")
       return    
     chat_id = chid
-    if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ **Tidak ada Lagu yang sedang diputar!**")
+    if chat_id not in callsmusic.active_chats:
+        await message.reply_text("❗ Nothing is streaming!")
     else:
         try:
-            callsmusic.queues.clear(chat_id)
+            queues.clear(chat_id)
         except QueueEmpty:
             pass
 
-        callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("❌ **Memberhentikan Lagu!**")
+        await callsmusic.stop(chat_id)
+        await message.reply_text("❌ Stopped streaming!")
 
 
 @Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
@@ -104,22 +107,28 @@ async def skip(_, message: Message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply("**Apakah obrolan terhubung?**")
+      await message.reply("Is chat even linked")
       return    
     chat_id = chid
-    if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ **Tidak ada Lagu Selanjutnya untuk dilewati!**")
+    if chat_id not in callsmusic.active_chats:
+        await message.reply_text("❗ Nothing is playing to skip!")
     else:
-        callsmusic.queues.task_done(chat_id)
+        queues.task_done(chat_id)
 
-        if callsmusic.queues.is_empty(chat_id):
-            callsmusic.pytgcalls.leave_group_call(chat_id)
+        if queues.is_empty(chat_id):
+            await callsmusic.stop(chat_id)
         else:
-            callsmusic.pytgcalls.change_stream(
-                chat_id, callsmusic.queues.get(chat_id)["file"]
+            await callsmusic.set_stream(
+                chat_id, 
+                queues.get(chat_id)["file"]
             )
 
-        await message.reply_text("⏩ **Melewati lagu saat ini!**")
+    qeue = que.get(chat_id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
 
 
 @Client.on_message(filters.command("channeladmincache"))
@@ -130,13 +139,16 @@ async def admincache(client, message: Message):
       conid = conchat.linked_chat.id
       chid = conid
     except:
-      await message.reply("**Apakah obrolan terhubung?**")
+      await message.reply("Is chat even linked")
       return
     set(
         chid,
-        [
+        (
             member.user
-            for member in await conchat.linked_chat.get_members(filter="administrators")
-        ],
+            for member in await conchat.linked_chat.get_members(
+                filter="administrators"
+            )
+        ),
     )
-    await message.reply_text("✅️ **Daftar admin** telah **diperbarui**")
+
+    await message.reply_text("❇️ Admin cache refreshed!")
