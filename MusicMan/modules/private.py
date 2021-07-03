3d165d8c0f5b39d9f8cc332120d1d91503a7a3db
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-03 10:10:01 +0700

    Update private.py

--- MusicMan/modules/private.py
+++ MusicMan/modules/private.py
@@ -24,7 +24,7 @@ logging.basicConfig(level=logging.INFO)
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
     client.send_message(message.chat.id,
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.my_name),
         parse_mode="markdown",
         reply_markup=InlineKeyboardMarkup(
             [
